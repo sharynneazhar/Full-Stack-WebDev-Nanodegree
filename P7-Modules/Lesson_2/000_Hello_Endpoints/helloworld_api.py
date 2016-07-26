e3dfe67f5be323f7da60f47beea7039b12e8b9fc
Sharynne Azhar <sharynneazhar@gmail.com>
2016-07-25 23:00:56 -0500

    add API to greet by name and time of day

--- P7-Modules/Lesson_2/000_Hello_Endpoints/helloworld_api.py
+++ P7-Modules/Lesson_2/000_Hello_Endpoints/helloworld_api.py
@@ -18,6 +18,12 @@ REQUEST_CONTAINER = endpoints.ResourceContainer(
     name=messages.StringField(1),
 )
 
+REQUEST_CONTAINER_2 = endpoints.ResourceContainer(
+    message_types.VoidMessage,
+    name=messages.StringField(1),
+    period=messages.StringField(2),
+)
+
 
 package = 'Hello'
 
@@ -40,6 +46,12 @@ class HelloWorldApi(remote.Service):
       path = "sayHelloByName", http_method='GET', name = "sayHelloByName")
     def say_hello_by_name(self, request):
       greet = "Hello {}".format(request.name)
+      return Hello(greeting=greet)
+
+    @endpoints.method(REQUEST_CONTAINER_2, Hello,
+      path = "greetByPeriod", http_method='GET', name = 'greetByPeriod')
+    def greetByPeriod(self, request):
+      greet = "Good {period}, {name}!".format(period=request.period, name=request.name)
       return Hello(greeting=greet)
 
 
