3212a18ccd006a6980c6d6a67f107bc2366f92e9
Sharynne Azhar <sharynneazhar@gmail.com>
2016-07-26 23:12:55 -0500

    feat: add ability to create conferences and filter them

---
+++ P7-Modules/Lesson_4/00_Conference_Central/settings.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+"""settings.py
+
+Udacity conference server-side Python App Engine app user settings
+
+$Id$
+
+created/forked from conference.py by wesc on 2014 may 24
+
+"""
+
+# Replace the following lines with client IDs obtained from the APIs
+# Console or Cloud Console.
+WEB_CLIENT_ID = '941028873246-lid5tp1e7tl4p3h7v01sjdcb2aj5a97g.apps.googleusercontent.com'
